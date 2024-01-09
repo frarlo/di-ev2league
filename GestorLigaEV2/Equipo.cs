5b560063e7dae5ac3fe0a06383d50b819be0e697
frarlo8 <farmentalo8@gmail.com>
2024-01-09 19:11:59 +0100

    basic structure

---
+++ GestorLigaEV2/Equipo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace GestorLigaEV2
+{
+    // Constructor del objeto tipo Equipo
+    class Equipo
+    {
+        // Atributos con getters y setters:
+        public string nombre { get; set; }
+        public int fundacion { get; set; }
+        public string estadio { get; set; }
+        public Image escudo { get; set; }
+    }
+}
---
+++ GestorLigaEV2/Jugador.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorLigaEV2
+{
+
+    // Constructor del objeto tipo Jugador
+    class Jugador
+    {
+        // Atributos con getters y setters:
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string apodo { get; set; }
+        public int edad {  get; set; }
+        public int dorsal {  get; set; }
+        public string nacionalidad { get; set; }
+        public Equipo equipo { get; set; }
+
+    }
+}
